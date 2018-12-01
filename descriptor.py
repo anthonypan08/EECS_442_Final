7071d618d7c5b98f2e11fd8c3456b8060c410885
yifanzhu <yifanzhu@umich.edu>
2018-12-01 16:52:12 -0500

    Change

--- descriptor.py
+++ descriptor.py
@@ -2,29 +2,24 @@ import cv2
 import numpy as np
 from skimage import feature
 
-'''
-Any time we want to obtain a feature vector from an image, 
-we can simply pass it to the object’s getFeatureVector() method, 
-which returns the feature vector.
-'''
 
 class Descriptor:
-    '''
-    This is a class that produce a feature vector for an input image.
-    '''
 
-    # input parameters for cv2
-    def __init__(self, hog_features=False, color_hist_features=False,
-                 spatial_features=False, hog_lib="cv", size=(64, 64),
+    """
+    Class that combines feature descriptors into a single descriptor
+    to produce a feature vector for an input image.
+    """
+
+    def __init__(self, hog_features=False, size=(64, 64),
                  hog_bins=9, pix_per_cell=(8, 8), cells_per_block=(2, 2),
                  block_stride=None, block_norm="L1", transform_sqrt=True,
-                 signed_gradient=False, hist_bins=16, spatial_size=(16, 16)):
-
+                 signed_gradient=False):
         """
         Set feature parameters. For HOG features, either the OpenCV
         implementation (cv2.HOGDescriptor) or scikit-image implementation
         (skimage.feature.hog) may be selected via @param hog_lib. Some
         parameters apply to only one implementation (indicated below).
+
         @param hog_features (bool): Include HOG features in feature vector.
         @param hist_features (bool): Include color channel histogram features
             in feature vector.
@@ -42,7 +37,7 @@ class Descriptor:
         @param block_norm: [scikit-image only] Block normalization method for
             HOG. OpenCV uses L2-Hys.
         @param transform_sqrt (bool): [scikit-image only].
-            @see skimage.feature.hog
+            @see skimage.feature.hog 
         @param hog_bins (int): Number of HOG gradient histogram bins.
         @param signed_gradient (bool): [OpenCV only] Use signed gradient (True)
             or unsigned gradient (False) for HOG. Currently, scikit-image HOG
@@ -52,50 +47,38 @@ class Descriptor:
             spatial binning.
         """
 
-
-        '''
-        block size is in pixels (not cells), 
-        and the block stride for block normalization must be also in pixels.
-        '''
         self.hog_features = hog_features
-        self.color_hist_features = color_hist_features
-        self.spatial_features = spatial_features
         self.size = size
-        self.hog_lib = hog_lib
         self.pix_per_cell = pix_per_cell
         self.cells_per_block = cells_per_block
 
-        window_size = size
-        cell_size = pix_per_cell
-        block_size = (cells_per_block[0] * cell_size[0],
-                         cells_per_block[1] * cell_size[1])
+        winSize = size
+        cellSize = pix_per_cell
+        blockSize = (cells_per_block[0] * cellSize[0],
+                        cells_per_block[1] * cellSize[1])
 
         if block_stride is not None:
-            stride_block = self.block_stride
+            blockStride = self.block_stride
         else:
-            stride_block = (int(block_size[0] / 2), int(block_size[1] / 2))
+            blockStride = (int(blockSize[0] / 2), int(blockSize[1] / 2))
 
-        # parameters to be tuned
-        nbins = hog_bins
-        derivAperture = 1
-        win_sigma = -1.
-        histogram_norm = 0  # L2Hys (currently the only available option)
-        L2HysThreshold = 0.2
-        gamma_correct = 1
-        levels = 64
-        gradients_signed = signed_gradient
+            nbins = hog_bins
+            derivAperture = 1
+            winSigma = -1.
+            # L2Hys (currently the only available option)
+            histogramNormType = 0
+            L2HysThreshold = 0.2
+            gammaCorrection = 1
+            nlevels = 64
+            signedGradients = signed_gradient
 
-        self.HOGDescriptor = cv2.HOGDescriptor(window_size, block_size,
-                                                   stride_block, cell_size, nbins, derivAperture, win_sigma,
-                                                   histogram_norm, L2HysThreshold, gamma_correct,
-                                                   levels, signed_gradient)
-
-        self.hist_bins = hist_bins
-        self.spatial_size = spatial_size
+            self.HOGDescriptor = cv2.HOGDescriptor(winSize, blockSize,
+                                                   blockStride, cellSize, nbins, derivAperture, winSigma,
+                                                   histogramNormType, L2HysThreshold, gammaCorrection,
+                                                   nlevels, signedGradients)
 
     def getFeatureVector(self, image):
-
-        '''Return the feature vector for an image.'''
+        """Return the feature vector for an image."""
 
         if image.shape[:2] != self.size:
             image = cv2.resize(image, self.size, interpolation=cv2.INTER_AREA)
@@ -105,32 +88,4 @@ class Descriptor:
         if self.hog_features:
             feature_vector = np.hstack(
                 (feature_vector, self.HOGDescriptor.compute(image)[:, 0]))
-
-        if self.color_hist_features:
-            # np.histogram() returns a tuple if given a 2D array and an array
-            # if given a 3D array. To maintain compatibility with other
-            # functions in the object detection pipeline, check that the input
-            # array has three dimensions. Add axis if necessary.
-            # Note that histogram bin range assumes uint8 array.
-            if len(image.shape) < 3:
-                image = image[:, :, np.newaxis]
-
-            '''
-            added color features and spital features
-            obtain color channel histogram features
-            '''
-            hist_vector = np.array([])
-            for channel in range(image.shape[2]):
-                channel_hist = np.histogram(image[:, :, channel],
-                                            bins=self.hist_bins, range=(0, 255))[0]
-                hist_vector = np.hstack((hist_vector, channel_hist))
-            feature_vector = np.hstack((feature_vector, hist_vector))
-
-        if self.spatial_features:
-            spatial_image = cv2.resize(image, self.spatial_size,
-                                       interpolation=cv2.INTER_AREA)
-            # flattened
-            spatial_vector = spatial_image.ravel()
-            feature_vector = np.hstack((feature_vector, spatial_vector))
-
         return feature_vector
--- extract_feature.py
+++ extract_feature.py
@@ -13,14 +13,13 @@ from descriptor import Descriptor
 
 def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
                  output_filename=None, color_space="bgr", channels=[0, 1, 2],
-                 hog_features=False, hist_features=False, spatial_features=False,
-                 hog_lib="cv", size=(64, 64), hog_bins=9, pix_per_cell=(8, 8),
+                 hog_features=False, size=(64, 64), hog_bins=9, pix_per_cell=(8, 8),
                  cells_per_block=(2, 2), block_stride=None, block_norm="L1",
-                 transform_sqrt=True, signed_gradient=False, hist_bins=16,
-                 spatial_size=(16, 16)):
+                 transform_sqrt=True, signed_gradient=False):
     """
     Extract features from positive samples and negative samples.
     Store feature vectors in a dict and optionally save to pickle file.
+
     @param pos_dir (str): Path to directory containing positive samples.
     @param neg_dir (str): Path to directory containing negative samples.
     @param recurse (bool): Traverse directories recursively (else, top-level only).
@@ -28,15 +27,19 @@ def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
     @param output_filename (str): Output file filename.
     @param color_space (str): Color space conversion.
     @param channels (list): Image channel indices to use.
-
+    
     For remaining arguments, refer to Descriptor class:
     @see descriptor.Descriptor#__init__(...)
+
     @return feature_data (dict): Lists of sample features split into training,
         cross-validation, test sets; scaler object; parameters used to
         construct descriptor and process images.
+
+    NOTE: OpenCV HOGDescriptor currently only supports 1-channel and 3-channel
+    images, not 2-channel images.
     """
 
-    if not (hog_features or hist_features or spatial_features):
+    if not (hog_features):
         raise RuntimeError("No features selected (set hog_features=True, "
                            + "hist_features=True, and/or spatial_features=True.)")
 
@@ -63,46 +66,37 @@ def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
     print("{} positive files and {} negative files found.\n".format(
         len(pos_files), len(neg_files)))
 
+    # Get color space information.
     color_space_name = "BGR"
     cv_color_const = -1
 
-    # Get names of desired features.
-    features = [feature_name for feature_name, feature_bool
-                in zip(["HOG", "color histogram", "spatial"],
-                       [hog_features, hist_features, spatial_features])
-                if feature_bool == True]
-
-    feature_str = features[0]
-    for feature_name in features[1:]:
-        feature_str += ", " + feature_name
-
+    # Get information about channel indices.
+    if len(channels) == 2 and hog_features and hog_lib == "cv":
+        warnings.warn("OpenCV HOG does not support 2-channel images",
+                      RuntimeWarning)
 
     channel_index_str = str(channels[0])
     for ch_index in channels[1:]:
         channel_index_str += ", {}".format(ch_index)
 
-    print("Converting images to " + color_space_name + " color space and "
-          + "extracting " + feature_str + " features from channel(s) "
-          + channel_index_str + ".\n")
-
-    # Store feature vectors for positive samples in list pos_features and
-    # for negative samples in neg_features.
     pos_features = []
     neg_features = []
     start_time = time.time()
 
     # Get feature descriptor object to call on each sample.
-    descriptor = Descriptor(hog_features=hog_features, hist_features=hist_features,
-                            spatial_features=spatial_features, hog_lib=hog_lib, size=size,
+    descriptor = Descriptor(hog_features=hog_features, size=size,
                             hog_bins=hog_bins, pix_per_cell=pix_per_cell,
                             cells_per_block=cells_per_block, block_stride=block_stride,
                             block_norm=block_norm, transform_sqrt=transform_sqrt,
-                            signed_gradient=signed_gradient, hist_bins=hist_bins,
-                            spatial_size=spatial_size)
+                            signed_gradient=signed_gradient)
 
     # Iterate through files and extract features.
     for i, filepath in enumerate(pos_files + neg_files):
         image = cv2.imread(filepath)
+        if image is None:
+            continue
+        if cv_color_const > -1:
+            image = cv2.cvtColor(image, cv_color_const)
 
         if len(image.shape) > 2:
             image = image[:, :, channels]
@@ -169,15 +163,12 @@ def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
         "neg_val": neg_val,
         "pos_test": pos_test,
         "neg_test": neg_test,
-        # "descriptor": descriptor,
+        #"descriptor": descriptor,
         "scaler": scaler,
         "hog_features": hog_features,
-        "hist_features": hist_features,
-        "spatial_features": spatial_features,
         "color_space": color_space,
         "cv_color_const": cv_color_const,
         "channels": channels,
-        "hog_lib": hog_lib,
         "size": size,
         "hog_bins": hog_bins,
         "pix_per_cell": pix_per_cell,
@@ -186,8 +177,6 @@ def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
         "block_norm": block_norm,
         "transform_sqrt": transform_sqrt,
         "signed_gradient": signed_gradient,
-        "hist_bins": hist_bins,
-        "spatial_size": spatial_size,
         "num_features": num_features
     }
 
@@ -203,3 +192,123 @@ def processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
     return feature_data
 
 
+def trainSVM(filepath=None, feature_data=None, C=1,
+             loss="squared_hinge", penalty="l2", dual=False, fit_intercept=False,
+             output_file=False, output_filename=None):
+    """
+        Train a classifier from feature data extracted by processFiles().
+
+        @param filepath (str): Path to feature data pickle file.
+        @param feature_data (dict): Feature data dict returned by processFiles().
+            NOTE: Either a file or dict may be supplied.
+        @param output_file (bool): Save classifier and parameters to file.
+        @param output_filename (str): Name of output file.
+
+        For remaining arguments, @see sklearn.svm.LinearSVC()
+
+        @return classifier_data (dict): Dict containing trained classifier and
+            relevant training/processing feature parameters.
+    """
+
+    print("Loading sample data.")
+    if filepath is not None:
+        filepath = os.path.abspath(filepath)
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError("File " + filepath + " does not exist.")
+        feature_data = pickle.load(open(filepath, "rb"))
+    elif feature_data is None:
+        raise ValueError("Invalid feature data supplied.")
+
+    # Train classifier on training set.
+    pos_train = np.asarray(feature_data["pos_train"])
+    neg_train = np.asarray(feature_data["neg_train"])
+    pos_val = np.asarray(feature_data["pos_val"])
+    neg_val = np.asarray(feature_data["neg_val"])
+    pos_test = np.asarray(feature_data["pos_test"])
+    neg_test = np.asarray(feature_data["neg_test"])
+
+    train_set = np.vstack((pos_train, neg_train))
+    train_labels = np.concatenate(
+        (np.ones(pos_train.shape[0],), np.zeros(neg_train.shape[0],)))
+
+    print("Training classifier...")
+    start_time = time.time()
+    classifier = svm.LinearSVC(C=C, loss=loss, penalty=penalty, dual=dual,
+                               fit_intercept=fit_intercept)
+    classifier.fit(train_set, train_labels)
+    print("Classifier trained in {:.1f} s.\n".format(time.time() - start_time))
+
+    # Run classifier on cross-validation set.
+    pos_val_predicted = classifier.predict(pos_val)
+    neg_val_predicted = classifier.predict(neg_val)
+
+    false_neg_val = np.sum(pos_val_predicted != 1)
+    false_pos_val = np.sum(neg_val_predicted == 1)
+    pos_predict_accuracy = 1 - (false_neg_val / float(pos_val.shape[0]))
+    neg_predict_accuracy = 1 - (false_pos_val / float(neg_val.shape[0]))
+    total_accuracy = 1 - ((false_neg_val + false_pos_val) /
+                          float(pos_val.shape[0] + neg_val.shape[0]))
+
+    print("Val set false negatives: {} / {} ({:.3}% accuracy)".format(
+        false_neg_val, pos_val.shape[0], 100 * pos_predict_accuracy))
+    print("Val set false positives: {} / {} ({:.3f}% accuracy)".format(
+        false_pos_val, neg_val.shape[0], 100 * neg_predict_accuracy))
+    print("Val set total misclassifications: {} / {} ({:.3f}% accuracy)\n".format(
+        false_neg_val + false_pos_val, pos_val.shape[0] + neg_val.shape[0],
+        100 * total_accuracy))
+
+    # Retrain classifier with misses from validation set. Run on test set.
+    print("Augmenting training set with misclassified validation samples and "
+          + "retraining classifier.\n")
+    pos_train = np.vstack((pos_train, pos_val[pos_val_predicted != 1, :]))
+    neg_train = np.vstack((neg_train, neg_val[neg_val_predicted == 1, :]))
+    train_set = np.vstack((pos_train, neg_train))
+    train_labels = np.concatenate(
+        (np.ones(pos_train.shape[0],), np.zeros(neg_train.shape[0],)))
+
+    classifier.fit(train_set, train_labels)
+
+    pos_test_predicted = classifier.predict(pos_test)
+    neg_test_predicted = classifier.predict(neg_test)
+
+    false_neg_test = np.sum(pos_test_predicted != 1)
+    false_pos_test = np.sum(neg_test_predicted == 1)
+    pos_predict_accuracy = 1 - (false_neg_test / float(pos_test.shape[0]))
+    neg_predict_accuracy = 1 - (false_pos_test / float(neg_test.shape[0]))
+    total_accuracy = 1 - ((false_neg_test + false_pos_test) /
+                          float(pos_test.shape[0] + neg_test.shape[0]))
+
+    print("Test set false negatives: {} / {} ({:.3}% accuracy)".format(
+        false_neg_test, pos_test.shape[0], 100 * pos_predict_accuracy))
+    print("Test set false positives: {} / {} ({:.3f}% accuracy)".format(
+        false_pos_test, neg_test.shape[0], 100 * neg_predict_accuracy))
+    print("Test set total misclassifications: {} / {} ({:.3f}% accuracy)".format(
+        false_neg_test + false_pos_test, pos_test.shape[0] + neg_test.shape[0],
+        100 * total_accuracy))
+
+    # Store classifier data and parameters in new dict that excludes
+    # sample data from feature_data dict.
+    excludeKeys = ("pos_train", "neg_train", "pos_val", "neg_val",
+                   "pos_test", "neg_test")
+    classifier_data = {key: val for key, val in feature_data.items()
+                       if key not in excludeKeys}
+    classifier_data["classifier"] = classifier
+
+    if output_file:
+        if output_filename is None:
+            output_filename = (datetime.now().strftime("%Y%m%d%H%M")
+                               + "_classifier.pkl")
+
+        pickle.dump(classifier_data, open(output_filename, "wb"))
+        print("\nSVM classifier data saved to {}".format(output_filename))
+
+    return classifier_data
+
+
+pos_dir = "data/BU101/images/Biking"
+neg_dir = "data/stanford40"
+processFiles(pos_dir, neg_dir, recurse=False, output_file=False,
+             output_filename=None, color_space="bgr", channels=[0, 1, 2],
+             hog_features=True, size=(64, 64), hog_bins=9, pix_per_cell=(8, 8),
+             cells_per_block=(2, 2), block_stride=None, block_norm="L1",
+             transform_sqrt=True, signed_gradient=False)
